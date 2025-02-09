1a87b5bc126b55820221d4b3e50e0c69a8112c8c
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-02-09 23:44:43 +0530

    starting with tic tac toe with react

---
+++ debug.js
@@ -0,0 +1,13 @@
+function hi(){
+    console.log("Hey,there");
+    console.log("How have you been?");  
+}
+
+let x=5;
+let y=10;
+hi();
+for(let i=0;i<4;i++){
+    x=x*2;
+    console.log(i);
+    console.log(x);
+}
